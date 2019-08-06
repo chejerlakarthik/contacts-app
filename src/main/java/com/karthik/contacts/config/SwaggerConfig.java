fe2b7a1a7678a158fc697fac4a65e7e03af1722c
Chejerla, Karthik <chejerla@au1.ibm.com>
2019-08-06 15:05:07 +1000

    Updated title

--- src/main/java/com/karthik/contacts/config/SwaggerConfig.java
+++ src/main/java/com/karthik/contacts/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.karthik.contacts.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +19,26 @@ public class SwaggerConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("com.karthik.contacts"))
+                .paths(PathSelectors.ant("/api/*"))
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                    .description("Simple Contact Management App")
+                    .title("Contacts API")
+                    .version("1.0")
+                    .contact(getApiContact())
+                    .build();
+    }
+
+    private Contact getApiContact() {
+        return new Contact(
+                    "Karthik Chejerla",
+                "https://github.com/chejerlakarthik",
+                "chejerlakarthik@gmail.com"
+            );
     }
 }
